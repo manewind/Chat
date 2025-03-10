7743e3f8111df0ab63ade5945380c87da926341f
manewind <tkachklim@gmail.com>
2025-03-10 14:06:46 +0300

    final_commit

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', (ws) => {
     console.log('Новое соединение установлено');
 
-    // Обработка сообщений от клиента
     ws.on('message', (message) => {
         const data = JSON.parse(message);
         console.log('Получено сообщение:', data);
 
-        // Отправка ответа всем клиентам
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(JSON.stringify({ text: data.text, isUser: false }));
@@ -17,7 +15,6 @@ wss.on('connection', (ws) => {
         });
     });
 
-    // Обработка закрытия соединения
     ws.on('close', () => {
         console.log('Соединение закрыто');
     });
